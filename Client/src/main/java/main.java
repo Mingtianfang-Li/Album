4afd1abbe000aca3735dcbb708b9062098a3c9dc
Ree Li <li.mingt@northeastern.edu>
2023-12-11 16:26:16 -0800

    add main

--- Client/src/main/java/main.java
+++ Client/src/main/java/main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class main {
+import io.swagger.client.ApiException;
+
+public class main {
+    public static void main(String[] args) throws ApiException {
+        SingleThreadClient singleThreadClient = new SingleThreadClient();
+        singleThreadClient.run();
+    }
 }
