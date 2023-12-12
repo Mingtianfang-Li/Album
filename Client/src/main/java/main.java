eb8413ebb1e5ab2dd6b70455d089598303a2dd6f
Ree Li <li.mingt@northeastern.edu>
2023-12-11 18:14:56 -0800

    add thread join and delay, also print wall time

--- Client/src/main/java/main.java
+++ Client/src/main/java/main.java
@@ -7,6 +7,7 @@ public class main {
     private static final int threadGroupSize = 10;
     private static final int numThreadGroups = 10;
     private static final int totalRequest = 100;
+    private static final int delay = 2;
 
     public static void main(String[] args) throws ApiException, InterruptedException {
         MultipleThread();
@@ -14,19 +15,33 @@ public class main {
 
     private static void MultipleThread(){
         long start = System.currentTimeMillis();
+        Thread[] threads = new Thread[numThreadGroups];
         Counter unsucessfulCounter = new Counter();
         for (int i = 0; i < numThreadGroups; i++){
-            for (int j = 0; j < threadGroupSize; j++){
-                Runnable worker = new Worker(unsucessfulCounter);
-                Thread thread = new Thread(worker);
-                thread.start();
-            }
+            threads[i] = new Thread(() -> {
+                Thread[] innerThreads = new Thread[threadGroupSize];
+                for (int j = 0; j < threadGroupSize; j++) {
+                    Runnable worker = new Worker(unsucessfulCounter);
+                    innerThreads[j] = new Thread(worker);
+                    innerThreads[j].start();
+                }
+                try {
+                    for (int j = 0; j < threadGroupSize; j++) {
+                        innerThreads[j].join();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            threads[i].start();
+            Thread.sleep(delay*1000);
         }
         long end = System.currentTimeMillis();
         double wallTime = (end - start) / 1000.0;
         int totalRequestsSent = threadGroupSize * numThreadGroups * totalRequest;
         int sucessfulCount = (totalRequestsSent - unsucessfulCounter.getVal());
         double throughput = totalRequestsSent / wallTime;
+        System.out.println("Wall Time: " + wallTime + " seconds");
         System.out.println("Throughput: " + throughput + " requests/second");
         System.out.println("Successful Requests: " + sucessfulCount);
         System.out.println("Failed Requests: " + unsucessfulCounter.getVal());
